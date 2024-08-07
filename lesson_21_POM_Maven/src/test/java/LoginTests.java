4a55f2f5ab1ac1e372450de0faac76e7259db6ea
NIKOLENKO <nikolenkote@gmail.com>
2024-08-07 14:36:34 +0200

    lesson 21

---
+++ lesson_21_POM_Maven/src/test/java/LoginTests.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+  @BeforeMethod
+  public void precondition() {
+    new HomePage(driver).getBookStore();
+    new SidePage(driver).selectLogin();
+  }
+
+  @Test
+  public void loginPositiveTest() {
+    new LoginPage(driver)
+        .enterPersonalData("root","Qwertyuiop@1")
+        .clickOnLoginButton()
+        .verifyUserName("root");
+  }
+}
