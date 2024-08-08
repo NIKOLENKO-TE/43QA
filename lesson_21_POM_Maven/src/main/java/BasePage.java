3eaea1851f0d9bf5b40d3ce09fa36df4667a6aff
NIKOLENKO <nikolenkote@gmail.com>
2024-08-08 07:12:56 +0200

    lesson 22 begin

--- lesson_21_POM_Maven/src/main/java/BasePage.java
+++ lesson_21_POM_Maven/src/main/java/BasePage.java
@@ -7,13 +7,16 @@ WebDriver driver;
 
   public BasePage(WebDriver driver) {
     this.driver = driver;
+    // PageFactory упрощает работу с веб-элементами и делает код более чистым и читаемым
+    // PageFactory — это утилита в Selenium WebDriver, которая упрощает инициализацию веб-элементов на странице и поддерживает концепцию Page Object Model (POM)
+    // PageFactory инициализирует элементы, помеченные аннотациями в вашем классе страницы
     PageFactory.initElements(driver, this);
   }
 
-  public void click(WebElement element){
+  // Реализуем методы клик и тайп чуть иначе потому что там мы принимали локаторы элементов, а теперь мы будем передавать в метод сразу элементы
+  public void click(WebElement element) {
     element.click();
   }
-
 
   public void type(WebElement element, String text) {
     if (text != null) {
