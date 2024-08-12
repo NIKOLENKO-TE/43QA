25f3e5684f84c03987939ef650c2c4480edb9b29
NIKOLENKO <nikolenkote@gmail.com>
2024-08-12 11:47:42 +0200

    lesson 24 begin

---
+++ lesson_24_POM_Select/src/main/java/demoqa/pages/SelectMenuPage.java
@@ -0,0 +1,56 @@
+package demoqa.pages;
+
+import demoqa.core.BasePage;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class SelectMenuPage extends BasePage {
+  public SelectMenuPage(WebDriver driver) {
+    super(driver);
+  }
+
+  @FindBy(id = "oldSelectMenu")
+  WebElement oldSelectMenu;
+
+  public SelectMenuPage selectOldStyle(String color) {
+    Select select = new Select(oldSelectMenu);
+    select.selectByVisibleText(color);
+    return this;
+  }
+
+  @FindBy(id = "react-select-4-input")
+  WebElement inputSelect;
+
+  @FindBy(css = "html")
+  WebElement space;
+
+  public SelectMenuPage multiSelect(String[] colors) {
+    for (int i = 0; i < colors.length; i++) {
+      if (colors[i] != null) {
+        inputSelect.sendKeys(colors[i]);
+        inputSelect.sendKeys(Keys.ENTER);
+      }
+      click(space);
+    }
+    return this;
+  }
+
+  @FindBy(id = "cars")
+  WebElement cars;
+
+  public SelectMenuPage standardMultiSelectByIndex(int index) {
+    Select select = new Select(cars);
+    if (select.isMultiple()) {
+      select.selectByIndex(index);
+    }
+    List<WebElement> options = select.getOptions();
+    String selectedText = options.get(index).getText();
+    System.out.println(selectedText);
+    return this;
+  }
+}
---
+++ lesson_24_POM_Select/src/test/java/demoqa/widgets/SelectMenuTests.java
@@ -0,0 +1,32 @@
+package demoqa.widgets;
+
+import demoqa.TestBase;
+import demoqa.pages.HomePage;
+import demoqa.pages.SelectMenuPage;
+import demoqa.pages.SidePage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SelectMenuTests extends TestBase {
+
+  @BeforeMethod
+  public void precondition() {
+    new HomePage(driver).getWidgets();
+    new SidePage(driver).selectSelectMenu();
+  }
+
+  @Test
+  public void oldStyleSelectMenuTest() {
+    new SelectMenuPage(driver).selectOldStyle("Indigo");
+  }
+
+  @Test
+  public void multiSelectMenuTest(){
+    new SelectMenuPage(driver).multiSelect(new String[]{"Green","Blue"});
+  }
+
+  @Test
+  public void standardMultiSelectTest(){
+    new SelectMenuPage(driver).standardMultiSelectByIndex(0);
+  }
+}
