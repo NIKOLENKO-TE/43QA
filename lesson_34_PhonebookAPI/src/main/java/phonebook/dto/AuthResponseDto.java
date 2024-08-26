3d6d9e56c6d95a832808fe19e8b9cb4ba5fdbfbf
NIKOLENKO <nikolenkote@gmail.com>
2024-08-26 13:16:27 +0200

    lesson 34

---
+++ lesson_34_PhonebookAPI/src/main/java/phonebook/dto/AuthResponseDto.java
@@ -0,0 +1,14 @@
+package phonebook.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class AuthResponseDto {
+  private String token;
+}
